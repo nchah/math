62169b770046333454707475550546720d8db35d
Niel C <nielchah@gmail.com>
2016-05-14 22:47:04 -0400

    Refactor printing to show direction

--- collatz.py
+++ collatz.py
@@ -6,17 +6,17 @@ import argparse
 def collatz(n):
     n = int(n)
     stack = ""
-    stack += str(n) + " -> "
+    stack += str(n)
     while n != 1:
         if n / 2 == 1:
             n = 1
-            stack += str(n)
+            stack += " => " + str(n)
         elif n % 2 == 0:
             n = n / 2
-            stack += str(n) + " -> "
+            stack += " +> " + str(n)
         elif n % 2 != 0 and n != 1:
             n = (n * 3) + 1
-            stack += str(n) + " -> "
+            stack += " -> " + str(n)
     print stack
 
 
