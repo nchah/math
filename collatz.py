d71fd8dd89e1d73f56dec9e1a976598636e80c92
Niel C <nielchah@gmail.com>
2016-05-14 22:48:26 -0400

    Fix direction

--- collatz.py
+++ collatz.py
@@ -13,10 +13,10 @@ def collatz(n):
             stack += " => " + str(n)
         elif n % 2 == 0:
             n = n / 2
-            stack += " +> " + str(n)
+            stack += " -> " + str(n)
         elif n % 2 != 0 and n != 1:
             n = (n * 3) + 1
-            stack += " -> " + str(n)
+            stack += " +> " + str(n)
     print stack
 
 
