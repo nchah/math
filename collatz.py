772a763fa72ad659716914e627d778d9fde5522f
Niel C <nielchah@gmail.com>
2016-05-19 15:28:04 -0400

    Adding # of ops

--- collatz.py
+++ collatz.py
@@ -11,19 +11,23 @@ import argparse
 
 def collatz(n):
     n = int(n)
+    stack_count = 0
     stack = ""
     stack += str(n)
     while n != 1:
         if n / 2 == 1:
             n = 1
             stack += " => " + str(n)
+            stack_count += 1
         elif n % 2 == 0:
             n = n / 2
             stack += " -> " + str(n)
+            stack_count += 1
         elif n % 2 != 0 and n != 1:
             n = (n * 3) + 1
             stack += " +> " + str(n)
-    print stack
+            stack_count += 1
+    print stack + " (" + str(stack_count) + " ops)"
 
 
 if __name__ == '__main__':
