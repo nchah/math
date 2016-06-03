efd9435c82439eaa83255bfe265004ed692b0f4d
Niel C <nielchah@gmail.com>
2016-06-02 22:32:39 -0400

    Java version of collatz

---
+++ scripts/Collatz.java
@@ -0,0 +1,16 @@
+/* 
+Collatz.java
+Run collatz calcuations on the command line.
+*/
+
+import java.util.Scanner;
+
+public class Collatz {
+    
+    public static void main (String[] args) {
+    
+
+    }
+}
+
+
